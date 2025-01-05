9fac46263ca3104b460825d095406e7592193d5e
wooo73 <ywaves073@gmail.com>
2025-01-06 00:11:40 +0900

    [chore]: class-validator, class- transformer 설치 및 적용

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaService } from './database/prisma/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
     const prismaService = app.get(PrismaService);
     await prismaService.enableShutdownHooks();
