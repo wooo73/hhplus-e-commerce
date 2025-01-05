9a489036cff246be76b71e8b3aaeda01974f1512
wooo73 <ywaves073@gmail.com>
2025-01-06 02:46:32 +0900

    [test]: Testcontainer prisma 적용
    - table 생성 및 seed 생성 prisma 적용

--- test/it/down.ts
+++ test/it/down.ts
@@ -1,8 +1,8 @@
-import { getDatasource } from './util';
+import { getPrismaClient } from './util';
 
 const down = async () => {
     await global.mysql.stop();
-    await (await getDatasource()).destroy();
+    await (await getPrismaClient()).$disconnect();
 };
 
 export default down;
--- test/it/import.sql
+++ test/it/import.sql
@@ -1,1 +1,2 @@
-SELECT 1;
+INSERT INTO user (id, balance, updated_at)
+VALUES(1, 1000, NOW());
