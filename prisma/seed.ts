b4a19247fd7a55669c64a777d89e234a7e031e72
wooo73 <ywaves073@gmail.com>
2025-01-08 01:00:32 +0900

    [fix]: 모델링 수정, seed 생성 수정
    - coupon - userCoupon 관계 설정, status 데이터 타입 변경
    - product - productQuantity명 변경
    - userCoupon - coupon, user 관계 설정 변경
    - user - userCoupon 관계 설정 변경

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { CouponStatus } from '../src/common/coupon.status';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await initTable();
-    await createMockData();
+    // await initTable();
+    // await createMockData();
+    // await createMockCouponAndUserCouponData();
 }
 
 async function initTable() {
@@ -19,17 +21,91 @@ async function initTable() {
 }
 
 async function createMockData() {
-    await prisma.user.create({
-        data: { balance: 10000 },
-    });
+    await Promise.all(
+        Array.from({ length: 10 }, () =>
+            prisma.user.create({
+                data: { balance: 10000 },
+            }),
+        ),
+    );
 
-    await prisma.product.create({
+    //사용자 생성
+    await Promise.all(
+        Array.from({ length: 10 }, () =>
+            prisma.user.create({
+                data: { balance: 10000 },
+            }),
+        ),
+    );
+
+    //상품 생성 및 재고 생성
+    for (const index of [...Array(30).keys()]) {
+        await prisma.product.create({
+            data: {
+                name: `테스트상품${index + 1}`,
+                price: Math.floor(Math.random() * 90001) + 10000, // 10000 ~ 100000 사이 랜덤 가격
+                productQuantity: {
+                    create: {
+                        quantity: 10,
+                        remainingQuantity: 10,
+                    },
+                },
+            },
+        });
+    }
+}
+
+//쿠폰 생성 및 재고 생성 & 유저 쿠폰 지급
+const createMockCouponAndUserCouponData = async () => {
+    const type = ['PERCENT', 'PRICE'];
+    const discountValue = [10, 2000];
+
+    const coupons = await Promise.all(
+        Array.from({ length: 2 }, (_, index) =>
+            prisma.coupon.create({
+                data: {
+                    name: `테스트쿠폰${index + 1}`,
+                    status: CouponStatus.AVAILABLE,
+                    discountType: type[index],
+                    discountValue: discountValue[index],
+                    startAt: new Date(),
+                    endAt: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 1),
+                    couponQuantity: {
+                        create: {
+                            quantity: 10,
+                            remainingQuantity: 10,
+                        },
+                    },
+                },
+            }),
+        ),
+    );
+
+    //쿠폰 지급 및 재고 차감
+    const user = await prisma.user.findFirst();
+
+    await prisma.couponQuantity.update({
+        where: {
+            couponId: coupons[0].id,
+        },
         data: {
-            name: '테스트 상품2',
-            price: 50000,
+            remainingQuantity: {
+                decrement: 1,
+            },
         },
     });
-}
+
+    await prisma.userCoupon.create({
+        data: {
+            user: {
+                connect: { id: user.id },
+            },
+            coupon: {
+                connect: { id: coupons[0].id },
+            },
+        },
+    });
+};
 
 main()
     .then(async () => {
