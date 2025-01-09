49561fca5bd17affcde7b4b9457c0108eb1b3e09
rlasuddn <ywaves073@gmail.com>
2025-01-10 03:07:05 +0900

    [feat]: 결제 기능 구현
    - facade 패턴 적용

---
+++ src/payment/application/payment.pacade.ts
@@ -0,0 +1,61 @@
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { UserService } from '../../user/domain/user.service';
+import { OrderService } from '../../order/domain/order.service';
+import { ProductService } from '../../product/domain/product.service';
+import { CouponService } from '../../coupon/domain/coupon.service';
+import {
+    TRANSACTION_MANAGER,
+    TransactionManager,
+} from '../../common/transaction/transaction-client';
+import { PaymentRequestDto } from '../presentation/dto/payment.request.dto';
+
+@Injectable()
+export class PaymentFacade {
+    constructor(
+        private readonly orderService: OrderService,
+        private readonly userService: UserService,
+        private readonly productService: ProductService,
+        private readonly couponService: CouponService,
+        @Inject(TRANSACTION_MANAGER) private readonly transactionManager: TransactionManager,
+    ) {}
+    async payment(dto: PaymentRequestDto) {
+        const { orderId, userId } = dto;
+
+        return await this.transactionManager.transaction(async (tx) => {
+            //주문 조회
+            const order = await this.orderService.getOrder(orderId, userId, tx);
+
+            //유저 조회
+            const user = await this.userService.findByIdWithLock(userId, tx);
+
+            //유저 잔고 검증
+            if (order.finalAmount > user.balance) {
+                throw new BadRequestException('잔액이 부족합니다.');
+            }
+
+            for (let item of order.orderItem) {
+                //주문 상품 재고 검증
+                await this.productService.validateProductRemainingQuantityWithLock(
+                    item.productId,
+                    item.quantity,
+                    tx,
+                );
+
+                //주문 상품 재고 차감
+                await this.productService.decreaseProductRemainingQuantity(
+                    item.productId,
+                    item.quantity,
+                    tx,
+                );
+            }
+
+            //결제 금액 차감
+            await this.userService.useUserBalance(userId, order.finalAmount, tx);
+
+            //주문 상태 변경
+            return await this.orderService.payOrder(orderId, tx);
+
+            //TODO: 외부 플랫폼 전송
+        });
+    }
+}
--- src/payment/presentation/dto/payment.request.dto.ts
+++ src/payment/presentation/dto/payment.request.dto.ts
@@ -1,3 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 import { OrderResponseDto } from 'src/order/presentation/dto/order.response.dto';
 
-export class PaymentRequestDto extends OrderResponseDto {}
+export class PaymentRequestDto {
+    @ApiProperty({ description: '주문 ID', example: 1 })
+    @IsNotEmpty()
+    @IsNumber()
+    orderId: number;
+
+    @ApiProperty({ description: '유저 ID', example: 1 })
+    @IsNotEmpty()
+    @IsNumber()
+    userId: number;
+}
