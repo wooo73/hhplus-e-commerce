e7e0a38dd13f3504467f8b194512bc2286cd7b9e
rlasuddn <ywaves073@gmail.com>
2025-01-16 21:09:09 +0900

    [fix]: user 통합 테스트 seed 생성 팩토리 함수 적용
    -

--- src/user/presentation/user.controller.it.spec.ts
+++ src/user/presentation/user.controller.it.spec.ts
@@ -5,25 +5,10 @@ import { USER_REPOSITORY } from '../domain/user.repository';
 import { UserPrismaRepository } from '../infrastructure/user.prisma.repository';
 import { PrismaService } from '../../database/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
-import { getPrismaClient } from '../../../test/it/util';
-import { PrismaClient } from '@prisma/client';
-import { UserDomain } from '../domain/user';
-
-let prisma: PrismaClient;
-
-const createMockUser = async () => {
-    prisma = await getPrismaClient();
-
-    const user = await prisma.user.create({
-        data: { balance: 10000 },
-    });
-
-    return user;
-};
+import { createMockUser } from '../../../prisma/seed/user.seed';
 
 describe('UserController', () => {
     let controller: UserController;
-    let userMockData: UserDomain;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -37,12 +22,13 @@ describe('UserController', () => {
         }).compile();
 
         controller = module.get<UserController>(UserController);
-        userMockData = await createMockUser();
     });
 
     it('SUCCESS_1번 사용자에 대한 충전이 정상 작동해야합니다.', async () => {
         //given
-        const userId = userMockData.id;
+        const balance = 10000;
+        const mockUser = await createMockUser(balance);
+        const userId = mockUser.id;
         const userChargePointRequestDto = {
             amount: 500,
         };
@@ -57,10 +43,13 @@ describe('UserController', () => {
 
     it('SUCCESS_1번 사용자가 동시에 충전을 진행하여도 순차적으로 작동해야합니다.', async () => {
         //given
-        const userId = userMockData.id;
+        const balance = 10000;
+        const mockUser = await createMockUser(balance);
+        const userId = mockUser.id;
         const userChargePointRequestDto = {
             amount: 500,
         };
+
         const callTimes = 5;
         const user = await controller.getUserBalance(userId);
 
