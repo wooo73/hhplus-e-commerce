c65a85035fbecd2c46ef249f41640521db0e8903
rlasuddn <ywaves073@gmail.com>
2025-01-23 21:01:24 +0900

    [feat]: 잔액 충전 redis 분산락(RedLock) 적용 API 추가
    - redis Redlock 적용 메서드 생성
    - 낙관적락, 분산락 비교 통합테스트 작성

--- src/user/presentation/user.controller.it.spec.ts
+++ src/user/presentation/user.controller.it.spec.ts
@@ -6,12 +6,16 @@ import { UserPrismaRepository } from '../infrastructure/user.prisma.repository';
 import { PrismaService } from '../../database/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { createMockUser } from '../../../prisma/seed/user.seed';
+import { RedisModule } from '../../database/redis/redis.module';
+import { RedisService } from '../../database/redis/redis.service';
 
 describe('UserController', () => {
     let controller: UserController;
+    let redisService: RedisService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [RedisModule],
             controllers: [UserController],
             providers: [
                 PrismaService,
@@ -22,6 +26,11 @@ describe('UserController', () => {
         }).compile();
 
         controller = module.get<UserController>(UserController);
+        redisService = module.get<RedisService>(RedisService);
+    });
+
+    afterEach(async () => {
+        await redisService.quit();
     });
 
     it('사용자에 대한 충전이 정상 작동한다.', async () => {
@@ -67,5 +76,60 @@ describe('UserController', () => {
 
         const user = await controller.getUserBalance(userId);
         expect(user.balance).toBe(mockUser.balance + userChargePointRequestDto.amount);
+    });
+
+    describe('잔액 충전 잠금 통합 테스트(중복 100번 동시 충전 요청)', () => {
+        const balance = 1500;
+        const callTimes = 100;
+
+        const userChargePointRequestDto = {
+            amount: 500,
+        };
+
+        it('낙관적락', async () => {
+            //given
+            const mockUser = await createMockUser(balance);
+            const userId = mockUser.id;
+
+            const chargePromise = Array.from({ length: callTimes }, () =>
+                controller.chargePoint(userId, userChargePointRequestDto),
+            );
+
+            //when
+            const result = await Promise.allSettled(chargePromise);
+
+            //then
+            const fulfilled = result.filter((p) => p.status === 'fulfilled');
+            const rejected = result.filter((p) => p.status === 'rejected');
+
+            expect(fulfilled.length).toEqual(1);
+            expect(rejected.length).toEqual(callTimes - 1);
+
+            const user = await controller.getUserBalance(userId);
+            expect(user.balance).toBe(mockUser.balance + userChargePointRequestDto.amount);
+        });
+
+        it('분산락', async () => {
+            //given
+            const mockUser = await createMockUser(balance);
+            const userId = mockUser.id;
+
+            const chargePromise = Array.from({ length: callTimes }, () =>
+                controller.chargePointWithRedis(userId, userChargePointRequestDto),
+            );
+
+            //when
+            const result = await Promise.allSettled(chargePromise);
+
+            //then
+            const fulfilled = result.filter((p) => p.status === 'fulfilled');
+            const rejected = result.filter((p) => p.status === 'rejected');
+
+            expect(fulfilled.length).toEqual(1);
+            expect(rejected.length).toEqual(callTimes - 1);
+
+            const user = await controller.getUserBalance(userId);
+            expect(user.balance).toBe(mockUser.balance + userChargePointRequestDto.amount);
+        });
     });
 });
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,10 @@ import { UserService } from './domain/user.service';
 import { UserController } from './presentation/user.controller';
 import { USER_REPOSITORY } from './domain/user.repository';
 import { UserPrismaRepository } from './infrastructure/user.prisma.repository';
+import { RedisModule } from '../database/redis/redis.module';
 
 @Module({
+    imports: [RedisModule],
     controllers: [UserController],
     providers: [UserService, { provide: USER_REPOSITORY, useClass: UserPrismaRepository }],
 })
