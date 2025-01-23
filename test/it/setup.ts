4039073b34850b4b4e0916bb8f506e8c76ed63c5
rlasuddn <ywaves073@gmail.com>
2025-01-23 16:48:31 +0900

    [test]: redis 테스트 컨테이너 세팅

--- test/it/setup.ts
+++ test/it/setup.ts
@@ -1,8 +1,9 @@
 import { MySqlContainer } from '@testcontainers/mysql';
+import { RedisContainer } from '@testcontainers/redis';
 import { execSync } from 'child_process';
 
 const init = async () => {
-    await Promise.all([initMysql()]);
+    await Promise.all([initMysql(), initRedis()]);
 };
 
 const initMysql = async () => {
@@ -16,21 +17,24 @@ const initMysql = async () => {
 
     process.env.DATABASE_URL = `mysql://${mysql.getUsername()}:${mysql.getUserPassword()}@${mysql.getHost()}:${mysql.getPort()}/${mysql.getDatabase()}`;
 
-    try {
-        // schema.prisma 기반으로 테이블 생성
-        execSync('npx prisma db push --force-reset', {
-            stdio: 'inherit',
-            env: { ...process.env },
-        });
+    // schema.prisma 기반으로 테이블 생성
+    execSync('npx prisma db push --force-reset', {
+        stdio: 'inherit',
+        env: { ...process.env },
+    });
 
-        execSync('npm run seed:test', {
-            stdio: 'inherit',
-            env: { ...process.env },
-        });
-    } catch (error) {
-        console.error('Migration failed:', error);
-        throw error;
-    }
+    execSync('npm run seed:test', {
+        stdio: 'inherit',
+        env: { ...process.env },
+    });
+};
+
+const initRedis = async () => {
+    const redis = await new RedisContainer('redis:7.0').withExposedPorts(6379).start();
+
+    process.env.REDIS_URL = `redis://localhost:${redis.getMappedPort(6379)}`;
+
+    global.redis = redis;
 };
 
 export default init;
