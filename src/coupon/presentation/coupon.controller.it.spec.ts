88914ed0f00b6125aa7a37c8087fbff4e5c4222a
rlasuddn <ywaves073@gmail.com>
2025-02-06 21:19:05 +0900

    [test]: 선착순 쿠폰 발급 레디스 자료구조 적용 동시성 테스트

--- src/coupon/presentation/coupon.controller.it.spec.ts
+++ src/coupon/presentation/coupon.controller.it.spec.ts
@@ -1,9 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
 
+import { LoggerModule } from '../../common/logger/logger.module';
 import { PrismaModule } from '../../database/prisma/prisma.module';
 import { CouponModule } from '../coupon.module';
 
-import { ConfigService } from '@nestjs/config';
 import { CouponController } from './coupon.controller';
 import { CouponService } from '../domain/coupon.service';
 
@@ -15,11 +16,17 @@ import { createMockCoupon } from '../../../prisma/seed/coupon.seed';
 import { RedisModule } from '../../database/redis/redis.module';
 import { RedlockService } from '../../database/redis/redlock.service';
 
-import { LoggerModule } from '../../common/logger/logger.module';
+import { CouponRedisRepository } from '../infrastructure/coupon.redis.repository';
+import {
+    generateIssueCouponKey,
+    generateRequestIssueCouponKey,
+} from '../../common/util/generateRedisKey';
 
 describe('CouponController', () => {
     let controller: CouponController;
+    let service: CouponService;
     let redlockService: RedlockService;
+    let couponRedisRepository: CouponRedisRepository;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -35,7 +42,9 @@ describe('CouponController', () => {
         }).compile();
 
         controller = module.get<CouponController>(CouponController);
+        service = module.get<CouponService>(CouponService);
         redlockService = module.get<RedlockService>(RedlockService);
+        couponRedisRepository = module.get<CouponRedisRepository>(CouponRedisRepository);
     });
 
     afterEach(async () => {
@@ -150,6 +159,54 @@ describe('CouponController', () => {
             //then
             expect(fulfilledResult.length).toEqual(remainingQuantity);
             expect(rejectedResult.length).toEqual(userCount - remainingQuantity);
+        });
+    });
+    describe('쿠폰 발급 요청 레디스 자료구조 적용', () => {
+        const userCount = 100;
+        const balance = 10000;
+        const type = 'PERCENT';
+        const discountValue = 10;
+        const remainingQuantity = 30;
+
+        it(`${userCount} 건의 동시 발급 요청(발급 가능 수량 ${remainingQuantity})`, async () => {
+            //given
+            const users = await Promise.all(
+                Array.from({ length: userCount }, () => createMockUser(balance)),
+            );
+
+            const coupon = await createMockCoupon(
+                CouponStatus.AVAILABLE,
+                type,
+                discountValue,
+                remainingQuantity,
+            );
+
+            const userIds = users.map((user) => user.id);
+            const couponId = coupon.id;
+
+            const couponKey = `coupon:${couponId}`;
+            await couponRedisRepository.setCouponStock(couponKey, remainingQuantity);
+
+            const length = userIds.length;
+
+            //when
+            const issueCouponPromise = Array.from({ length: length }, (_, index) =>
+                controller.issueCouponRequestEnqueue(couponId, userIds[index]),
+            );
+
+            await Promise.all(issueCouponPromise);
+            await service.issueCouponRequestDequeue(couponId);
+
+            //then
+            const requestKey = generateRequestIssueCouponKey(couponId);
+            const issueKey = generateIssueCouponKey(couponId);
+
+            const requestQueueCnt =
+                await couponRedisRepository.issueCouponRequestQueueCnt(requestKey);
+            const issueQueueCnt = await couponRedisRepository.IssueCouponQueueCnt(issueKey);
+
+            expect(requestQueueCnt).toEqual(userCount - remainingQuantity);
+            expect(issueQueueCnt).toEqual(remainingQuantity);
         });
     });
 });
