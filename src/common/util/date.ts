eaa1bb5fb83baadf4193dcca1116a3535c4ca3e3
rlasuddn <ywaves073@gmail.com>
2025-02-20 18:05:01 +0900

    [feat]: 분 조정 일자 함수 생성

--- src/common/util/date.ts
+++ src/common/util/date.ts
@@ -5,6 +5,10 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+export const convertDayjs = (date: Date | string) => {
+    return dayjs(date).tz('Asia/Seoul');
+};
+
 export const getCurrentDate = (format: string = 'YYYY-MM-DD') => {
     const currentDate = dayjs().tz('Asia/Seoul').startOf('day').format(format);
     return currentDate;
@@ -12,7 +16,12 @@ export const getCurrentDate = (format: string = 'YYYY-MM-DD') => {
 
 export const getPastDate = (day: number, format: string = 'YYYY-MM-DD') => {
     const currentDate = getCurrentDate();
-    const pastDate = dayjs(currentDate).subtract(day, 'day').startOf('day').format(format);
+    const pastDate = dayjs(currentDate).subtract(day, 'day').format(format);
+    return pastDate;
+};
+
+export const getPastMinuteDate = (minute: number): dayjs.Dayjs => {
+    const pastDate = dayjs().tz('Asia/Seoul').subtract(minute, 'minutes');
     return pastDate;
 };
 
