2a24c67649ae72294765b29b49ec8026e70813a0
rlasuddn <ywaves073@gmail.com>
2025-02-27 17:13:44 +0900

    [fix]: 트랜잭션 설정
    - prisma transaction max wait, time out 설정

--- src/common/transaction/prisma.transaction-client.ts
+++ src/common/transaction/prisma.transaction-client.ts
@@ -7,9 +7,15 @@ export class PrismaTransactionManager implements TransactionManager {
     constructor(private readonly prisma: PrismaService) {}
 
     async transaction<T>(action: (tx: TransactionClient) => Promise<T>): Promise<T> {
-        return await this.prisma.$transaction((prismaTx) => {
-            const client: TransactionClient = { prisma: prismaTx };
-            return action(client);
-        });
+        return await this.prisma.$transaction(
+            (prismaTx) => {
+                const client: TransactionClient = { prisma: prismaTx };
+                return action(client);
+            },
+            {
+                maxWait: 10000, // 10초
+                timeout: 30000, // 30초
+            },
+        );
     }
 }
